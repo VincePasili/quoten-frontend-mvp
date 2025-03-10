0e2bc9d05e7c6b6b91510447951ddef8bd253dc3
godfreykaris <137221176+godfreykaris@users.noreply.github.com>
2025-03-11 00:12:47 +0300

    Update api.js

--- src/utilities/api.js
+++ src/utilities/api.js
@@ -43,7 +43,7 @@ const secureFetch = async (url, options = {}) => {
 
   if (response.status === 403) {
     //deleteCSRFToken();
-    window.location.href = process.env.REACT_APP_SIGNIN_ROUTE;
+    // window.location.href = process.env.REACT_APP_SIGNIN_ROUTE;
     return; // Prevent further execution
   }
 
